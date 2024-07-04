631c23aa9c85bbf84174930534cd5a3b12aabb04
Guillaume Oblin <guillaume.oblin@sfr.fr>
2024-07-04 21:05:02 +0200

    nouveau toto

--- app/pages/models.py
+++ app/pages/models.py
@@ -74,3 +74,10 @@ class ContactSubmission(models.Model):
 
     def __str__(self):
         return f"{self.last_name} {self.first_name}"
+
+    def nouveau_toto():
+        pass
+
+    def nouveau_stata():
+        # Nouvelle fonction ajoutÃ©e
+        pass
